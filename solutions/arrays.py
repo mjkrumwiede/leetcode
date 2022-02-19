f9d2edab95ba0b949ec29bb2298486a25ae4e131
Matthew Krumwiede <mjkrumwiede@gmail.com>
2022-02-18 17:33:54 -0700

    Added more solutions. Removed self argument from static methods

--- solutions/arrays.py
+++ solutions/arrays.py
@@ -12,7 +12,7 @@ class ArraySolutions():
             d[num] = index
     
     @staticmethod
-    def removeDuplicates(self, nums: List[int]) -> int:
+    def removeDuplicates(nums: List[int]) -> int:
         slow = 0
         last_num = 101
         for fast in range(len(nums)):
@@ -23,7 +23,7 @@ class ArraySolutions():
         return slow
     
     @staticmethod
-    def searchInsert(self, nums: List[int], target: int) -> int:
+    def searchInsert(nums: List[int], target: int) -> int:
         length = len(nums)
         if length == 1:
             return 1 if target > nums[0] else 0
@@ -34,3 +34,70 @@ class ArraySolutions():
             return self.searchInsert(nums[:middle_index], target)
         else:
             return middle_index + self.searchInsert(nums[middle_index:], target)
+
+    @staticmethod
+    def plusOne(digits: List[int]) -> List[int]:
+        ind = len(digits) - 1
+        digits[ind] += 1
+        while(digits[ind] > 9):
+            if ind == 0:
+                digits[ind] = 0
+                digits.insert(0,1)
+                break
+            else:
+                digits[ind] = 0
+                ind -= 1
+                digits[ind] += 1
+        return digits
+    
+    @staticmethod
+    def maxProfit(prices: List[int]) -> int:
+        max_profit, min_price = 0, float('inf')
+        for price in prices:
+            min_price = min(min_price, price)
+            profit = price - min_price
+            max_profit = max(max_profit, profit)
+        return max_profit
+
+    @staticmethod
+    def singleNumber(nums: List[int]) -> int:
+        for i in range(1, len(nums)):
+            nums[0]^=nums[i]
+        return nums[0]
+    
+    @staticmethod
+    def majorityElement(nums: List[int]) -> int:
+        table = {}
+        for num in nums:
+            if num in table:
+                table[num] = table[num] + 1
+            else:
+                table[num] = 1
+        return max(table, key=table.get)
+
+    @staticmethod
+    def moveZeroes(nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        length = len(nums)
+        index = 0
+        while(index<length):
+            if nums[index] == 0:
+                del nums[index]
+                nums.append(0)
+                length -= 1
+            else:
+                index += 1
+
+    @staticmethod
+    def findDisappearedNumbers(nums: List[int]) -> List[int]:
+        n = len(nums)
+        n_set = set()
+        for num in nums:
+            n_set.add(num)
+        missing = []
+        for num in range(1,n+1):
+            if num not in n_set:
+                missing.append(num)
+        return missing
